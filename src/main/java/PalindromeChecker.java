77af5ea449ef135fe92845f099194c545958db5a
willy2day <143228693+willy2day@users.noreply.github.com>
2024-02-06 18:08:59 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -25,7 +25,7 @@ public void tester()
   System.out.println("there are " + lines.length + " lines");
   for (int i=0; i < lines.length; i++) 
   {
-    if(palindrome(lines[i])==true)
+    if(isPalindrome(lines[i])==true)
     {
       System.out.println(lines[i] + " IS a palindrome.");
     }
